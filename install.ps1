9f9335eb08300c339622348ba2e8e1ffb94bbe15
Bruno Souto <65253099+rhattox@users.noreply.github.com>
2025-09-01 21:26:54 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,6 +5,7 @@ $packages = @(
     @{ Id = "Google.Chrome";                     Version = "" }
     @{ Id = "7zip.7zip";                         Version = "" }
     @{ Id = "Logitech.GHUB";                     Version = "" }
+    @{ Id = "VideoLAN.VLC";                      Version = "" }
     @{ Id = "Insomnia.Insomnia";                 Version = "" }
     @{ Id = "Notepad++.Notepad++";               Version = "" }
     @{ Id = "Flameshot.Flameshot";               Version = "" }
@@ -49,4 +50,5 @@ Write-Host "Installation complete. Please restart your computer if prompted." -F
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
 winget install --id=DEVCOM.JetBrainsMonoNerdFont  -e
+
 
