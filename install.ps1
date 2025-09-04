d11ebb94a80a80ce82f8877586ff8e331ed3ec62
Bruno Souto <65253099+rhattox@users.noreply.github.com>
2025-09-03 21:37:42 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -6,6 +6,7 @@ $packages = @(
     @{ Id = "7zip.7zip";                         Version = "" }
     @{ Id = "Logitech.GHUB";                     Version = "" }
     @{ Id = "VideoLAN.VLC";                      Version = "" }
+    @{ Id = "git.git";                           Version = "" }
     @{ Id = "Insomnia.Insomnia";                 Version = "" }
     @{ Id = "Notepad++.Notepad++";               Version = "" }
     @{ Id = "Flameshot.Flameshot";               Version = "" }
@@ -50,5 +51,6 @@ Write-Host "Installation complete. Please restart your computer if prompted." -F
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
 winget install --id=DEVCOM.JetBrainsMonoNerdFont  -e
+
 
 
